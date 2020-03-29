ae56312745dc8d385c338afe3ef70ac0eabf7701
Adam Drake <addrake@ebay.com>
2020-03-29 16:53:46 +0200

    updating for simple tests

--- simple.js
+++ simple.js
@@ -1,16 +1,33 @@
-const sum = (a, b) => a + b;
-const subtract = (a, b) => a - b;
+const { sum, subtract } = require("./math");
 
-let result = sum(3, 7);
-let expected = 10;
+test("sum add numbers", () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`);
+test("subtract subtracts number", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`Yea! ${title}`);
+  } catch (error) {
+    console.log(`Boo! ${title}`);
+    console.log(error);
+  }
 }
 
-result = subtract(7, 3);
-expected = 4;
-
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`);
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    }
+  };
 }
